1b5a1765169e8c714ef4e93c8ca4b4f34e8cd26a
Anthony Picciano <anthony.picciano@gmail.com>
2012-12-17 14:35:27 -0700

    Initial Commit

---
+++ SlideOutNavigationProto1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideOutNavigationProto1
+//
+//  Created by Anthony Picciano on 12/17/12.
+//  Copyright (c) 2012 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideOutNavigationProto1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlideOutNavigationProto1
+//
+//  Created by Anthony Picciano on 12/17/12.
+//  Copyright (c) 2012 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
